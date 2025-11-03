5dd0f3e2fd9f6d3c773ed0d29f3f963c85f1195b
yumejensen <yumejensen@gmail.com>
2025-11-03 13:04:24 -0600

    Added 8 way movement and sprite flip to bat, changed default bg color to bluish grey

--- assets/scripts/bat.gd
+++ assets/scripts/bat.gd
@@ -1,25 +1,24 @@
 extends CharacterBody2D
 
+# movement speed
+@export var speed = 750
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+# function to receive input - 8 way movement
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
 
-
+# set a variable for the animated sprite (got by holding control and drag)	
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+	
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	get_input()
 	move_and_slide()
+	
+	# set a variable to get right or left 
+	var direction = Input.get_axis("left", "right")
+	# flip the sprite left or right based on direction
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
